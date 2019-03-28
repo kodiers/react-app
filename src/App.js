1abb291ca881251d35a6624886821fd127a013a9
Victor Yamchinov <kodiers@gmail.com>
2019-03-28 05:22:12 +0300

    Revert "deleted radium"
    This reverts commit 1f2f684d

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, {StyleRoot} from 'radium';
 
 import Person from './Person/Person';
 
@@ -45,7 +46,11 @@ class App extends Component {
         font: 'inherit',
         border: '1px solid blue',
         padding: '8px',
-        cursor: 'pointer'
+        cursor: 'pointer',
+        ':hover': {
+          backgroundColor: 'lightgreen',
+          color: 'black'
+        }
       };
       let persons = null;
 
@@ -63,6 +68,10 @@ class App extends Component {
           </div>
         );
         style.backgroundColor = 'red';
+        style[':hover'] = {
+          backgroundColor: 'salmon',
+          color: 'black'
+        }
       }
       const classes = [];
       if (this.state.persons.length <= 2) {
@@ -72,14 +81,16 @@ class App extends Component {
         classes.push('bold');
       }
       return (
-        <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working.</p>
-          <button onClick={() => this.togglePersonsHandler()} style={style}>Toggle persons</button>
-          {persons}
-        </div>
+        <StyleRoot>
+          <div className="App">
+            <h1>Hi, I'm a React App</h1>
+            <p className={classes.join(' ')}>This is really working.</p>
+            <button onClick={() => this.togglePersonsHandler()} style={style}>Toggle persons</button>
+            {persons}
+          </div>
+        </StyleRoot>
       );
   }
 }
 
-export default App;
+export default Radium(App);
